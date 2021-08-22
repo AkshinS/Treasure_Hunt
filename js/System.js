04f3723f392ebda1bd9ba808bfc46399d67d7f26
AkshinS <72020729+AkshinS@users.noreply.github.com>
2021-08-22 19:33:52 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,16 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+
+        if (actualCode === enteredCode.toUpperCase() || actualCode === enteredCode.toLowerCase())
+            return true
+        else
+            return false
+    }
+
+}
